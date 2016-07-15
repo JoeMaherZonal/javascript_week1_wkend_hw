5b2f86f94f4b3f8a77e18032b3f8b5f56b3ce533
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-07-15 11:14:47 +0100

    initial commit:

---
+++ specs/record_store_specs.js
@@ -0,0 +1,16 @@
+var RecordStore = require('../record_store');
+// var Account = require('../bank').account;
+var assert = require('chai').assert;
+
+
+describe("Bank", function(){
+
+  beforeEach(function(){
+    //refresh here
+  })
+
+  it('check record store has been initiated', function(){
+    assert.deepEqual(0, 0)
+  })
+
+})
